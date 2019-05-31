e19b7e260fdeee15350ae29f46d651255876d836
tuananht4qh <tuananht4qh@gmail.com>
2019-05-31 23:15:39 +0700

    add lần đầu

--- DoAnTotNghiep/Form1.Designer.cs
+++ DoAnTotNghiep/Form1.Designer.cs
@@ -29,38 +29,52 @@
         private void InitializeComponent()
         {
             this.lb1 = new System.Windows.Forms.Label();
-            this.lb2 = new System.Windows.Forms.Label();
+            this.lb4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.anhdauvao = new System.Windows.Forms.PictureBox();
-            this.anhdaura = new System.Windows.Forms.PictureBox();
+            this.anhgoc = new System.Windows.Forms.PictureBox();
+            this.anhxam = new System.Windows.Forms.PictureBox();
+            this.MausangXam = new System.Windows.Forms.Button();
+            this.lb2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.anhamban = new System.Windows.Forms.PictureBox();
+            this.anhxuly = new System.Windows.Forms.PictureBox();
+            this.XamsangAmBan = new System.Windows.Forms.Button();
+            this.lb3 = new System.Windows.Forms.Label();
+            this.zGHistogram = new ZedGraph.ZedGraphControl();
             this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.anhdauvao)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.anhdaura)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.anhgoc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.anhxam)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.anhamban)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.anhxuly)).BeginInit();
             this.SuspendLayout();
             // 
             // lb1
             // 
             this.lb1.AutoSize = true;
-            this.lb1.Location = new System.Drawing.Point(329, 9);
+            this.lb1.Location = new System.Drawing.Point(303, 12);
             this.lb1.Name = "lb1";
             this.lb1.Size = new System.Drawing.Size(47, 13);
             this.lb1.TabIndex = 1;
             this.lb1.Text = "Ảnh gốc";
             // 
-            // lb2
+            // lb4
             // 
-            this.lb2.AutoSize = true;
-            this.lb2.Location = new System.Drawing.Point(610, 9);
-            this.lb2.Name = "lb2";
-            this.lb2.Size = new System.Drawing.Size(120, 13);
-            this.lb2.TabIndex = 2;
-            this.lb2.Text = "Ảnh sau khi tăng cường";
+            this.lb4.AutoSize = true;
+            this.lb4.Location = new System.Drawing.Point(577, 569);
+            this.lb4.Name = "lb4";
+            this.lb4.Size = new System.Drawing.Size(131, 13);
+            this.lb4.TabIndex = 2;
+            this.lb4.Text = "Ảnh sau khi gian mức xám";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(28, 42);
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(28, 45);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
@@ -68,67 +82,165 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // button3
             // 
-            this.button2.Location = new System.Drawing.Point(28, 113);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 46);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Xử lý tăng cường ảnh";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button3.Location = new System.Drawing.Point(28, 221);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 46);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Xử lý tăng cường ảnh";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button2_Click);
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.anhdauvao, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.anhdaura, 1, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(210, 42);
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.8195F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.1805F));
+            this.tableLayoutPanel1.Controls.Add(this.anhgoc, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.anhxam, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(152, 25);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(607, 314);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(650, 270);
             this.tableLayoutPanel1.TabIndex = 6;
             // 
-            // anhdauvao
+            // anhgoc
             // 
-            this.anhdauvao.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.anhdauvao.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.anhdauvao.Location = new System.Drawing.Point(3, 3);
-            this.anhdauvao.Name = "anhdauvao";
-            this.anhdauvao.Size = new System.Drawing.Size(297, 308);
-            this.anhdauvao.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.anhdauvao.TabIndex = 0;
-            this.anhdauvao.TabStop = false;
+            this.anhgoc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.anhgoc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.anhgoc.Location = new System.Drawing.Point(3, 3);
+            this.anhgoc.Name = "anhgoc";
+            this.anhgoc.Size = new System.Drawing.Size(317, 264);
+            this.anhgoc.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.anhgoc.TabIndex = 0;
+            this.anhgoc.TabStop = false;
             // 
-            // anhdaura
+            // anhxam
             // 
-            this.anhdaura.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.anhdaura.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.anhdaura.Location = new System.Drawing.Point(306, 3);
-            this.anhdaura.Name = "anhdaura";
-            this.anhdaura.Size = new System.Drawing.Size(298, 308);
-            this.anhdaura.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.anhdaura.TabIndex = 1;
-            this.anhdaura.TabStop = false;
+            this.anhxam.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.anhxam.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.anhxam.Location = new System.Drawing.Point(326, 3);
+            this.anhxam.Name = "anhxam";
+            this.anhxam.Size = new System.Drawing.Size(321, 264);
+            this.anhxam.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.anhxam.TabIndex = 1;
+            this.anhxam.TabStop = false;
+            // 
+            // MausangXam
+            // 
+            this.MausangXam.Location = new System.Drawing.Point(28, 94);
+            this.MausangXam.Name = "MausangXam";
+            this.MausangXam.Size = new System.Drawing.Size(75, 40);
+            this.MausangXam.TabIndex = 7;
+            this.MausangXam.Text = "Màu sang xám";
+            this.MausangXam.UseVisualStyleBackColor = true;
+            this.MausangXam.Click += new System.EventHandler(this.MausangXam_Click);
+            // 
+            // lb2
+            // 
+            this.lb2.AutoSize = true;
+            this.lb2.Location = new System.Drawing.Point(622, 12);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(48, 13);
+            this.lb2.TabIndex = 8;
+            this.lb2.Text = "Ảnh xám";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.8195F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.1805F));
+            this.tableLayoutPanel2.Controls.Add(this.anhamban, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.anhxuly, 1, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(152, 298);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(650, 264);
+            this.tableLayoutPanel2.TabIndex = 9;
+            // 
+            // anhamban
+            // 
+            this.anhamban.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.anhamban.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.anhamban.Location = new System.Drawing.Point(3, 3);
+            this.anhamban.Name = "anhamban";
+            this.anhamban.Size = new System.Drawing.Size(317, 258);
+            this.anhamban.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.anhamban.TabIndex = 0;
+            this.anhamban.TabStop = false;
+            // 
+            // anhxuly
+            // 
+            this.anhxuly.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.anhxuly.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.anhxuly.Location = new System.Drawing.Point(326, 3);
+            this.anhxuly.Name = "anhxuly";
+            this.anhxuly.Size = new System.Drawing.Size(321, 258);
+            this.anhxuly.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.anhxuly.TabIndex = 1;
+            this.anhxuly.TabStop = false;
+            // 
+            // XamsangAmBan
+            // 
+            this.XamsangAmBan.Location = new System.Drawing.Point(28, 152);
+            this.XamsangAmBan.Name = "XamsangAmBan";
+            this.XamsangAmBan.Size = new System.Drawing.Size(75, 48);
+            this.XamsangAmBan.TabIndex = 10;
+            this.XamsangAmBan.Text = "Xám sang âm bản";
+            this.XamsangAmBan.UseVisualStyleBackColor = true;
+            this.XamsangAmBan.Click += new System.EventHandler(this.Gray_Click);
+            // 
+            // lb3
+            // 
+            this.lb3.AutoSize = true;
+            this.lb3.Location = new System.Drawing.Point(303, 569);
+            this.lb3.Name = "lb3";
+            this.lb3.Size = new System.Drawing.Size(64, 13);
+            this.lb3.TabIndex = 11;
+            this.lb3.Text = "Ảnh âm bản";
+            // 
+            // zGHistogram
+            // 
+            this.zGHistogram.Location = new System.Drawing.Point(805, 28);
+            this.zGHistogram.Name = "zGHistogram";
+            this.zGHistogram.ScrollGrace = 0D;
+            this.zGHistogram.ScrollMaxX = 0D;
+            this.zGHistogram.ScrollMaxY = 0D;
+            this.zGHistogram.ScrollMaxY2 = 0D;
+            this.zGHistogram.ScrollMinX = 0D;
+            this.zGHistogram.ScrollMinY = 0D;
+            this.zGHistogram.ScrollMinY2 = 0D;
+            this.zGHistogram.Size = new System.Drawing.Size(510, 531);
+            this.zGHistogram.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(846, 450);
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1327, 591);
+            this.Controls.Add(this.zGHistogram);
+            this.Controls.Add(this.lb3);
+            this.Controls.Add(this.XamsangAmBan);
+            this.Controls.Add(this.tableLayoutPanel2);
             this.Controls.Add(this.lb2);
+            this.Controls.Add(this.MausangXam);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lb4);
             this.Controls.Add(this.lb1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.tableLayoutPanel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.anhdauvao)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.anhdaura)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.anhgoc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.anhxam)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.anhamban)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.anhxuly)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -136,12 +248,20 @@
 
         #endregion
         private System.Windows.Forms.Label lb1;
-        private System.Windows.Forms.Label lb2;
+        private System.Windows.Forms.Label lb4;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.PictureBox anhdauvao;
-        private System.Windows.Forms.PictureBox anhdaura;
+        private System.Windows.Forms.PictureBox anhgoc;
+        private System.Windows.Forms.Button MausangXam;
+        private System.Windows.Forms.Label lb2;
+        private System.Windows.Forms.PictureBox anhxam;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.PictureBox anhamban;
+        private System.Windows.Forms.PictureBox anhxuly;
+        private System.Windows.Forms.Button XamsangAmBan;
+        private System.Windows.Forms.Label lb3;
+        private ZedGraph.ZedGraphControl zGHistogram;
     }
 }
 
--- DoAnTotNghiep/Form1.cs
+++ DoAnTotNghiep/Form1.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using ZedGraph;
 namespace DoAnTotNghiep
 {
     public partial class Form1 : Form
@@ -18,21 +20,289 @@ namespace DoAnTotNghiep
             InitializeComponent();
         }
 
-       
 
-        private void button2_Click(object sender, EventArgs e)
+        public void button2_Click(object sender, EventArgs e)
         {
+            Bitmap b = (Bitmap)anhxam.Image.Clone();
+            b = ContrastStretch(b);
+            //b = Normalization(b);
+            anhxuly.Image = b;
+            //Tính histogram
+            double[] histogram = TinhHistogram(b);
+            //Chuyển đổi kiểu dữ liệu
+            PointPairList points = ChuyenDoiHistogram(histogram);
+            //Vẽ biểu đồ histogram và cho hiển thị
+            zGHistogram.GraphPane = BieuDoHistogram(points);
+            zGHistogram.Refresh();
+
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        public void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog open = new OpenFileDialog();
-            if(open.ShowDialog()== System.Windows.Forms.DialogResult.OK)
+            if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 image1 = new Bitmap(open.FileName);
-                anhdauvao.Image = image1;
+                anhgoc.Image = image1;
             }
+        }
+
+       /* public void Button3_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        protected void Lb2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }*/
+        private void MausangXam_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = (Bitmap)anhgoc.Image.Clone();
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+                    int t = (int)(p.R * 0.299 + p.G * 0.587 + p.B * 0.114);
+                    bmp.SetPixel(x, y, Color.FromArgb(t, t, t));
+
+                }
+            }
+            anhxam.Image = bmp;
+            //Tính histogram
+            double[] histogram = TinhHistogram(bmp);
+            //Chuyển đổi kiểu dữ liệu
+            PointPairList points = ChuyenDoiHistogram(histogram);
+            //Vẽ biểu đồ histogram và cho hiển thị
+            zGHistogram.GraphPane = BieuDoHistogram(points);
+            zGHistogram.Refresh();
+        }
+        public void Gray_Click(object sender, EventArgs e)
+        {
+            Bitmap kq = (Bitmap)anhxam.Image.Clone();
+            for (int y = 0; y < kq.Height; y++)
+            {
+                for (int x = 0; x < kq.Width; x++)
+                {
+                    Color q = kq.GetPixel(x, y);
+                    kq.SetPixel(x, y, Color.FromArgb(255 - q.R, 255 - q.G, 255 - q.B));
+
+                }
+            }
+            anhamban.Image = kq;
+            //Tính histogram
+            double[] histogram = TinhHistogram(kq);           
+            //Chuyển đổi kiểu dữ liệu
+            PointPairList points = ChuyenDoiHistogram(histogram);
+            //Vẽ biểu đồ histogram và cho hiển thị
+            zGHistogram.GraphPane = BieuDoHistogram(points);
+            zGHistogram.Refresh();
+
+        }
+        //Tính histogram của hình mức xám
+        double[] TinhHistogram(Bitmap hinhmucxam)
+        {
+            double[] histogram = new double[256];
+            for (int x = 0; x < hinhmucxam.Width; x++)
+                for (int y = 0; y < hinhmucxam.Height; y++)
+                {
+                    Color color = hinhmucxam.GetPixel(x, y);
+                    byte gray = color.R;
+                    histogram[gray]++;
+                }
+            return histogram;
+        }
+
+        PointPairList ChuyenDoiHistogram(double[] histogram)
+        {
+            //PointPairList là kiểu dữ liệu của zedgraph để vẽ biểu đồ
+            PointPairList point = new PointPairList();
+            for (int i = 0; i < histogram.Length; i++)
+            {
+                //i tương ứng với trục nằm ngang, từ 0-255
+                //histogram[i] tương ứng với trục đứng, là số pixel có cùng mức xám 
+                point.Add(i, histogram[i]);
+            }
+            return point;
+        }
+        //Thiết lập biểu đồ trong zedGraph
+        GraphPane BieuDoHistogram(PointPairList histogram)
+        {
+            //GraphPane là đối tượng biểu dồ trong ZedGraph
+            GraphPane gp = new GraphPane();
+            gp.Title.Text = @"Biểu đồ Histogram";//Tên của biểu đồ
+            gp.Rect = new RectangleF(0, 0, 512, 536);//Khung chứa biểu đố
+
+            //THiết lập trục ngang
+            gp.XAxis.Title.Text = @"Giá trị mức xám của điểm ảnh";
+            gp.XAxis.Scale.Min = 0; //nhỏ nhất là 0
+            gp.XAxis.Scale.Max = 255; // lớn nhất là 255
+            gp.XAxis.Scale.MajorStep = 5; //mỗi bước chính là 5
+            gp.XAxis.Scale.MinorStep = 1;// Mỗi bước trong một bước chính là 1
+
+            //Thiết lập trục đứng
+            gp.YAxis.Title.Text = @"Số điểm ảnh có cùng mức xám";
+            gp.YAxis.Scale.Min = 0; //nhỏ nhất là 0
+            gp.YAxis.Scale.Max = 83886; // số này phải lớn hơn kich thước của ảnh
+            gp.YAxis.Scale.MajorStep = 5; //mỗi bước chính là 5
+            gp.YAxis.Scale.MinorStep = 1;// Mỗi bước trong một bước chính là 1
+
+            //Dùng biểu đồ dạng bar để biểu diễn Histogram
+            gp.AddBar("Histogram", histogram, Color.AliceBlue);
+            return gp;
+        }
+        public static Bitmap ContrastStretch(Bitmap srcImage,double blackPointPercent = 0.03, double whitePointPercent = 0.02)
+        /*Trong đó: -blackPointPercent là % pixel tối nhất chuyển sang màu đen
+                    -whitePointPercent là % pixel sáng nhất chuyển sang màu trắng.
+        */
+        {
+            //truy cập đến điểm ảnh:Truy cập từ con trỏ đầu tiên
+            //lock bit cho hình ảnh nguồn của bạn vào bộ nhớ hệ thống
+            Rectangle rect = new Rectangle(0, 0, srcImage.Width, srcImage.Height);
+            BitmapData srcData = srcImage.LockBits(rect, ImageLockMode.ReadOnly,PixelFormat.Format32bppArgb);
+
+            //Tạo một bitmap mà bạn sẽ viết dữ liệu pixel mới
+            Bitmap destImage = new Bitmap(srcImage.Width, srcImage.Height);
+
+            //Lock bits cho bitmap có thể ghi của bạn vào bộ nhớ hệ thống
+            Rectangle rect2 = new Rectangle(0, 0, destImage.Width, destImage.Height);
+            BitmapData destData = destImage.LockBits(rect2, ImageLockMode.WriteOnly,PixelFormat.Format32bppArgb);
+
+            //Lấy chiều rộng của một hàng pixel trong bitmap
+            int stride = srcData.Stride;
+
+            //Quét các dòng đầu tiên trong bitmap
+            IntPtr srcScan0 = srcData.Scan0;
+            IntPtr destScan0 = destData.Scan0;
+
+            var freq = new int[256];
+
+            unsafe
+            {
+                //Tạo một mảng dữ liệu pixel từ hình ảnh nguồn
+                byte* src = (byte*)srcScan0;
+
+                //Lấy số lượng pixel cho mỗi giá trị cường độ
+                for (int y = 0; y < srcImage.Height; ++y)
+                {
+                    for (int x = 0; x < srcImage.Width; ++x)
+                    {
+                        freq[src[y * stride + x * 4]]++;
+                    }
+                }
+
+                //Lấy tổng số pixel trong ảnh
+                int numPixels = srcImage.Width * srcImage.Height;
+
+                //Đặt giá trị cường độ tối thiểu của hình ảnh (0 = đen)
+                int minI = 0;
+
+                //Lấy tổng số pixel màu đen
+                var blackPixels = numPixels * blackPointPercent;
+
+                //Đặt một biến để tổng hợp các pixel sẽ chuyển sang màu đen
+                int accum = 0;
+
+                //Tổng hợp các màu tối nhất cho đến khi bạn đạt được tổng số pixel đen
+                while (minI < 255)
+                {
+                    accum += freq[minI];
+                    if (accum > blackPixels) break;
+                    minI++;
+                }
+
+                //Đặt cường độ tối đa của hình ảnh (255 = trắng)
+                int maxI = 255;
+
+                //Đặt tổng số pixel trắng
+                var whitePixels = numPixels * whitePointPercent;
+
+                //Đặt lại biến tổng trở về 0
+                accum = 0;
+
+                //Tổng hợp các pixel sáng nhất sẽ chuyển sang màu trắng
+                while (maxI > 0)
+                {
+                    accum += freq[maxI];
+                    if (accum > whitePixels) break;
+                    maxI--;
+                }
+
+                //Một phần của phương trình chuẩn hóa không thay đổi theo từng pixel
+                double spread = 255d / (maxI - minI);
+
+                //Tạo một mảng dữ liệu pixel từ hình ảnh được tạo
+                byte* dst = (byte*)destScan0;
+
+                //Viết dữ liệu pixel mới vào hình ảnh
+                for (int y = 0; y < srcImage.Height; ++y)
+                {
+                    for (int x = 0; x < srcImage.Width; ++x)
+                    {
+                        int i = y * stride + x * 4;
+
+                        //Một phần của phương trình thay đổi theo từng pixel
+                        double value = Math.Round((src[i] - minI) * spread);
+
+                        byte val = (byte)(Math.Min(Math.Max(value, 0), 255));
+                        dst[i] = val;
+                        dst[i + 1] = val;
+                        dst[i + 2] = val;
+                        dst[i + 3] = 255;
+                    }
+                }
+            }
+
+            //Unlock bits from system memory
+            srcImage.UnlockBits(srcData);
+            destImage.UnlockBits(destData);
+
+            return destImage;
+        }
+        public static Bitmap Normalization(Bitmap img)
+        {
+            int w = img.Width;
+            int h = img.Height;
+
+            BitmapData sd = img.LockBits(new Rectangle(0, 0, w, h),ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            int bytes = sd.Stride * sd.Height;
+            byte[] buffer = new byte[bytes];
+            byte[] result = new byte[bytes];
+            Marshal.Copy(sd.Scan0, buffer, 0, bytes);
+            img.UnlockBits(sd);
+            int current = 0;
+            byte max = 0;
+            byte min = 255;
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                max = Math.Max(max, buffer[i]);
+                min = Math.Min(min, buffer[i]);
+            }
+            for (int y = 0; y < h; y++)
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    current = y * sd.Stride + x * 4;
+                    for (int i = 0; i < 3; i++)
+                    {
+                        result[current + i] = (byte)((buffer[current + i] - min) * 100 / (max - min));
+                    }
+                    result[current + 3] = 255;
+                }
+            }
+            Bitmap resimg = new Bitmap(w, h);
+            BitmapData rd = resimg.LockBits(new Rectangle(0, 0, w, h),
+                ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+            Marshal.Copy(result, 0, rd.Scan0, bytes);
+            resimg.UnlockBits(rd);
+            return resimg;
         }
     }
 }
